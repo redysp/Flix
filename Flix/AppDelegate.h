a199290158d57145cf1ce1d7ed3eedbecee5348c
Carlos Power <powercarlos25@fb.com>
2019-06-26 11:12:20 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by powercarlos25 on 6/26/19.
+//  Copyright © 2019 powercarlos25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by powercarlos25 on 6/26/19.
+//  Copyright © 2019 powercarlos25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
